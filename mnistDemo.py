10df9f892d92bc10abc79eae0cfd26ef4ce653e8
Jie Tao <taoj@mail.gvsu.edu>
2018-05-23 18:37:53 -0400

    v1.1
    mnistDemo functional prepared

--- mnistDemo.py
+++ mnistDemo.py
@@ -3,31 +3,33 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 # get data from library which includes train, validation and test
 # use one-hot coding for output
-minist = input_data.read_data_sets("/home/jie/tensorflow/temp/",one_hot=True)
+mnist = input_data.read_data_sets("/home/jie/tensorflow/temp/",one_hot=True)
 
 # setting ANN variables
 structure = [784,500,500,10]
 x = tf.placeholder(dtype='float',shape=[None,structure[0]])
-y = tf.placeholder(dtype='float',shape=structure[-1])
+y = tf.placeholder(dtype='float')
 batch_size = 100
+epoch_max = 10
 
 # feedforward Model
 def netural_network_model(x):
+    # l is output value from active function
     l = 0
     l_prev = 0
     for i in range(1,len(structure)):
-        # general layer structure
+        # create each layer structure
         hidden_layer = {'weight':tf.Variable(tf.random_normal([structure[i-1],structure[i]])),
                         'biases':tf.Variable(tf.random_normal([structure[i]]))}
-        # input layer
+        # input layer--> first hidden layer
         if i == 1:
             l = tf.add(tf.matmul(x, hidden_layer['weight']),hidden_layer['biases'])
             l = tf.nn.relu(l)
             l_prev = l
-        # output layer
+        # --->output layer without active function
         elif i == len(structure)-1:
             l = tf.add(tf.matmul(l_prev, hidden_layer['weight']), hidden_layer['biases'])
-        # hidden layer
+        # hidden layer ---> hidden layer with Relu active function
         else:
             l = tf.add(tf.matmul(l_prev, hidden_layer['weight']), hidden_layer['biases'])
             l = tf.nn.relu(l)
@@ -35,30 +37,32 @@ def netural_network_model(x):
     return l
 
 def train_model(x):
+    # prediction, cost and gradient decrease
     prediction = netural_network_model(x)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
-    epoch_max = 10
 
-    # initial all variables
     with tf.Session() as sess:
+        # create log for flow chart
         writer = tf.summary.FileWriter("/tmp/log/...", sess.graph)
-
+        # initial all variables
         sess.run(tf.global_variables_initializer())
-
+        # start epoch loop
         for epoch in range(epoch_max):
             epoch_loss = 0
-            for _ in range(int(minist.train.num_examples/batch_size)):
-                epoch_x, epoch_y = minist.train.next_batch(batch_size)
+            # mini batch
+            for _ in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                # run session
                 _, c = sess.run([optimizer,cost],feed_dict={x:epoch_x,y:epoch_y})
                 epoch_loss += c
-                print('Epoch', epoch, 'completed out of', epoch_max, 'loss:', epoch_loss)
+            print('Epoch', epoch, 'completed out of', epoch_max, 'loss:', epoch_loss)
 
+        # evaluation with test data
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', accuracy.eval({x: minist.train.images, y: minist.test.labels}))
-    writer.close()
+        print('Accuracy:', accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+        writer.close()
 
 train_model(x)
